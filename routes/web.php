9bbfd4a95a7befbf3e74d87517f61164a294af0a
DrewL64 <shillongtitude@gmail.com>
2025-01-16 11:20:41 +0530

    Added Demo COntroller and Demo Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'AboutMethod')->name('about.page')->middleware('checkage');
+    Route::get('/contact','ContactMethod')->name('contact.page');
 });
